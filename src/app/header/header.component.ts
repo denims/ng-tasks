b0f85164006a073d9e3dce85b696c0690fb5a8a0
denims <denimallel@gmail.com>
2024-12-28 15:51:28 +0530

    Added styles to header component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -4,6 +4,7 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
+  imports: [],
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {}
