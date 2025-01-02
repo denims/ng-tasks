0f17868463efa954cd34f5cdb5c7b65db6f3aa53
denims <denimallel@gmail.com>
2025-01-02 14:53:08 +0530

    Added signals to user component

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,26 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, computed, signal} from '@angular/core';
 import {DUMMY_USERS} from '../dummy-users';
-import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-user',
-  imports: [
-    NgOptimizedImage
-  ],
+  imports: [],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
 export class UserComponent {
   randomIndex = Math.floor(Math.random() * (DUMMY_USERS.length));
-  selectedUser = DUMMY_USERS[this.randomIndex];
+  selectedUser = signal(DUMMY_USERS[this.randomIndex]);
+  imagePath = computed(() => '../../assets/users/' + this.selectedUser().avatar);
 
-
-  get imagePath() {
-    return '../../assets/users/' + this.selectedUser.avatar;
-  }
 
   onUserSelected() {
     this.randomIndex = Math.floor(Math.random() * (DUMMY_USERS.length));
-    this.selectedUser = DUMMY_USERS[this.randomIndex];
+    this.selectedUser.set(DUMMY_USERS[this.randomIndex]);
   }
 }
