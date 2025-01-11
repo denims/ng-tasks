97cd153dcb5081e1bdb372444e0b12812d46805d
denims <denimallel@gmail.com>
2025-01-11 13:01:27 +0530

    Added tasks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,23 @@
-import {Component} from '@angular/core';
+import {Component, signal} from '@angular/core';
 import {HeaderComponent} from './header/header.component';
 import {UserComponent} from './user/user.component';
 import {DUMMY_USERS} from "./dummy-users";
 import {NgForOf} from "@angular/common";
+import {TasksComponent} from './tasks/tasks.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, UserComponent, NgForOf],
+  imports: [HeaderComponent, UserComponent, NgForOf, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   users = DUMMY_USERS;
   title = 'deni-2';
+  selectedUser = signal<string>('Select a User');
 
   onUserSelected(userId: string) {
-    console.log(userId);
+    this.selectedUser.set(this.users.find(e => e.id === userId)!.name);
   }
 }
 
